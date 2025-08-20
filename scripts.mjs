17085c0b9f73731db97006efdd1bc03c3450d922
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-08-20 15:32:36 -0400

    part 3 added

--- scripts.mjs
+++ scripts.mjs
@@ -23,30 +23,30 @@ adventurer.companion = {
    // The companion’s name is “Frank.”
    // The companion’s type is “Flea.”
    // The companion has its own belongings, which includes a small hat and sunglasses.
-adventurer.companion.companion = {
-    name: "Frank",
-    type: "Flea",
-    belongings: ["hat", "sunglasses"]
-}
+// adventurer.companion.companion = {
+//     name: "Frank",
+//     type: "Flea",
+//     belongings: ["hat", "sunglasses"]
+// }
 
-console.log(adventurer)
+// console.log(adventurer)
 
 // Give Robin the following method:
 
-adventurer.roll = function (mod = 0) {
-    const result = Math.floor(Math.random() *20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`)
-}
+// adventurer.roll = function (mod = 0) {
+//     const result = Math.floor(Math.random() *20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`)
+// }
 // Now we have a method for “dice rolls, a common way to handle outcomes in adventuring games. Test this method by calling adventurer.roll() a few times.
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
+// adventurer.roll();
+// adventurer.roll();
+// adventurer.roll();
 
 //================================End of Part 1 ==============================
 
 // Part 2. Classy Fantasy
 class Character {
-    constructor (name, roll) {
+    constructor (name, health, inventory, roll) {
         this.name = name,
         this.health = 100,
         this.inventory = []
@@ -59,35 +59,34 @@ class Character {
 }
 
 // Now, we can re-create Robin using the Character class!
-const robin = new Character ("Robin");
-console.log(`The new class BEFORE adding the relevant data for each character looks like the following:\n`, robin)
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// const robin = new Character ("Robin");
+// console.log(`The new class BEFORE adding the relevant data for each character looks like the following:\n`, robin)
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-console.log(`The new class AFTER adding the relevant data for each character looks like the following:\n`, robin);
+// console.log(`The new class AFTER adding the relevant data for each character looks like the following:\n`, robin);
 
 
 // Test the roll funciton for each character
-console.log("Let's make each character use the roll function: ")
-robin.roll();
-robin.companion.roll();
-robin.companion.companion.roll();
+// console.log("Let's make each character use the roll function: ")
+// robin.roll();
+// robin.companion.roll();
+// robin.companion.companion.roll();
 
 //================================End of Part 2 ==============================
 
 // Part 3: Class Features
+
 // Take a look at our example below, and expand upon it with your own properties and methods. What else should an adventurer be able to do? What other properties should they have?
 // Answer: health, companion from the parent class
 
 class Adventurer extends Character {
-    constructor (name, role) {
-    super(name);
-    super(health); // they need health from the Character class
-    super(companion); // they need companion from the Character class
+    constructor (name, health, companion, role) {
+    super(name, health, companion);
     // Adventurers have specialized roles.
     this.role = role;
     // Every adventurer starts with a bed and 50 gold coins.
@@ -102,15 +101,16 @@ class Adventurer extends Character {
 
 
 // Next, create a Companion class with properties and methods specific to the companions.
-class Companion extends Companion {
+class Companion extends Character {
     constructor (name, health, companion, inventory, role) {
-        super(name);
-        super(health); // they need health from the Character class
-        super(companion); // they need companion from the Character class
-        super(inventory)
+        super(name, health, companion, inventory);
         this.role = role;
-        super.roll();
     }
+    scout () {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+    }
+    
 }
 
 // Finally, change the declaration of Robin and the companions to use the new Adventurer and Companion classes. NOT CLEAR TO ME!
@@ -125,3 +125,7 @@ robin.companion.companion.type = "Flea";
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 console.log(`The new class AFTER adding the relevant data for each character looks like the following:\n`, robin);
+
+//================================End of Part 3 ==============================
+
+// Part 4: Class Uniforms
